1b14e8e2a02c8a2c32018d59c458b4ff46716c9c
Nathan Smith <nathan@nathansmith.io>
2017-12-23 10:23:15 -0800

    Good filtering with match-sorter

--- src/views/components/SourceTable/index.js
+++ src/views/components/SourceTable/index.js
@@ -1,31 +1,49 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import matchSorter from 'match-sorter';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
 import './style.scss';
 
+function filterMethod(filter, rows) {
+  return matchSorter(rows, filter.value, {
+    keys: ['name', 'org', 'phone', 'email'],
+  });
+}
+
 const SourceTable = props => {
   const sourcesToRender = props.allSourcesQuery.allSources;
   const columns = [
-    { Header: 'Name', accessor: 'name' },
-    { Header: 'Organization', accessor: 'org', minWidth: 200 },
+    { Header: 'Name', accessor: 'name', filterMethod, filterAll: true },
+    {
+      Header: 'Organization',
+      accessor: 'org',
+      minWidth: 200,
+      filterMethod,
+      filterAll: true,
+    },
     {
       Header: 'Phone',
       accessor: 'phone',
       Cell: props => <a href={`tel:+1-${props.value}`}>{props.value}</a>,
+      filterMethod,
+      filterAll: true,
     },
     {
       Header: 'Email',
       accessor: 'email',
       Cell: props => <a href={`mailto:${props.value}`}>{props.value}</a>,
+      filterMethod,
+      filterAll: true,
     },
-    { Header: 'Notes', accessor: 'notes', minWidth: 200 },
+    { Header: 'Notes', accessor: 'notes', minWidth: 200, filterAll: true },
   ];
 
   return (
     <div>
       <ReactTable
+        filterable
         data={sourcesToRender}
         columns={columns}
         defaultPageSize={50}
