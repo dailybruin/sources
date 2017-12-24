78bd50a3fe868b28034e29208cf977a9ca326af1
Nathan Smith <nathan@nathansmith.io>
2017-12-24 13:02:46 -0800

    Search works!

--- src/views/components/SourceTable/index.js
+++ src/views/components/SourceTable/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
 import matchSorter from 'match-sorter';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
+
 import './style.scss';
 
 function filterMethod(filter, rows) {
@@ -12,69 +11,70 @@ function filterMethod(filter, rows) {
   });
 }
 
-const SourceTable = props => {
-  const sourcesToRender = props.allSourcesQuery.allSources;
-  const columns = [
-    { Header: 'Name', accessor: 'name', filterMethod, filterAll: true },
+class SourceTable extends React.Component {
+  columns = [
+    {
+      Header: 'Name',
+      id: 'name',
+      accessor: 'name',
+      filterMethod,
+      filterAll: true,
+    },
     {
       Header: 'Organization',
       accessor: 'org',
       minWidth: 200,
-      filterMethod,
-      filterAll: true,
-      filterable: false,
     },
     {
       Header: 'Phone',
       accessor: 'phone',
       Cell: props => <a href={`tel:+1-${props.value}`}>{props.value}</a>,
-      filterMethod,
-      filterAll: true,
-      filterable: false,
     },
     {
       Header: 'Email',
       accessor: 'email',
       Cell: props => <a href={`mailto:${props.value}`}>{props.value}</a>,
-      filterMethod,
-      filterAll: true,
-      filterable: false,
     },
     {
       Header: 'Notes',
       accessor: 'notes',
       minWidth: 200,
-      filterAll: true,
-      filterable: false,
     },
   ];
 
-  return (
-    <div>
-      <ReactTable
-        filterable
-        data={sourcesToRender}
-        columns={columns}
-        defaultPageSize={50}
-        className="-striped -highlight"
-      />
-    </div>
-  );
-};
-
-const ALL_SOURCES_QUERY = gql`
-  query AllSourcesQuery {
-    allSources {
-      name
-      org
-      phone
-      email
-      notes
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
   }
-`;
 
-// 3
-export default graphql(ALL_SOURCES_QUERY, { name: 'allSourcesQuery' })(
-  SourceTable
-);
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+    return this.refs.reactTable.filterColumn(
+      this.columns[0],
+      event.target.value
+    );
+  };
+
+  render() {
+    return (
+      <div className="source-table">
+        <input
+          type="text"
+          name="filter"
+          placeholder="Search"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <ReactTable
+          ref="reactTable"
+          data={this.props.sources}
+          columns={this.columns}
+          defaultPageSize={50}
+          className="-striped -highlight"
+        />
+      </div>
+    );
+  }
+}
+
+export default SourceTable;
