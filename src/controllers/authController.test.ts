d48fb3fb0ba8b414e7dae49aa714d50b77ebe59c
Nathan Smith <nathan@nathansmith.io>
2017-12-24 17:01:21 -0800

    Add some test files

---
+++ src/controllers/authController.test.ts
@@ -0,0 +1,15 @@
+import * as authController from './authController';
+
+describe('redirectToAuthURL', () => {
+  it('should redirect to a Google generated URL', () => {});
+});
+
+// describe('authCallback', () => {
+//   // it('should redirect to a Google generated URL', () => {});
+//   // it('should redirect to a Google generated URL', () => {});
+// });
+
+describe('ensureAuthenticated', () => {
+  it('should return next() for authenticated users');
+  it('should redirect unauthenticated users to the login page');
+});
