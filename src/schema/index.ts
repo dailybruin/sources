f656cb728a159bc5a5c7d004f4d45320ad0f6e9a
Nathan Smith <nathan@nathansmith.io>
2017-12-25 21:10:41 -0800

    Use .graphql file

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,29 +1,45 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import { makeExecutableSchema } from 'graphql-tools';
-import { resolvers } from './resolvers';
 
+import { readAllSources, createSource } from '../controllers/sourceController';
+
+const typeDefs = [
+  fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
+];
 // Define your types here.
-const typeDefs = `
-  type Source {
-    id: ID!
-    name: String!,
-    organization: String,
-    phone: String,
-    email: String,
-    notes: String,
-  }
+// const typeDefs = `
+//   type Source {
+//     id: ID!
+//     name: String!,
+//     organization: String,
+//     phone: String,
+//     email: String,
+//     notes: String,
+//   }
 
-  type Query {
-    allSources: [Source!]!
-  }
+//   type Query {
+//     allSources: [Source!]!
+//   }
 
-  type Mutation {
-    addSource(
-      name: String!,
-      organization: String,
-      phone: String,
-      email: String,
-      notes: String): Source
-  }
-`;
+//   type Mutation {
+//     addSource(
+//       name: String!,
+//       organization: String,
+//       phone: String,
+//       email: String,
+//       notes: String): Source
+//   }
+// `;
+
+const resolvers = {
+  Query: {
+    allSources: readAllSources,
+  },
+
+  Mutation: {
+    addSource: createSource,
+  },
+};
 
 export const schema = makeExecutableSchema({ typeDefs, resolvers });
