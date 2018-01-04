48b14bfb0242a952c13729774b954854e0eff78c
Nathan Smith <nathan@nathansmith.io>
2018-01-04 10:56:15 -0800

    Fix authController bug with .hasOwnProperty
    Apparently `query` doesn’t inherit the object prototype?

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -20,9 +20,12 @@ export function redirectToAuthURL(
 }
 
 export function authCallback(req: Request, res: Response, next: NextFunction) {
-  const authURL = url.parse(req.url, true) as any; // get authentication code
+  const authURL = url.parse(req.url, true); // get authentication code
 
-  if (!authURL.query || !authURL.query.hasOwnProperty('code')) {
+  if (
+    !authURL.query ||
+    !Object.prototype.hasOwnProperty.call(authURL.query, 'code')
+  ) {
     res.redirect('/login');
   } else {
     oauth2Client.getToken(authURL.query.code, (err, tokens) => {
