dad413cfed7e03d26dd8888d86547917530cdf48
Nathan Smith <nathan@nathansmith.io>
2018-01-15 08:37:16 -0800

    Cleaner stuff

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,7 @@ import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import * as bodyParser from 'body-parser';
 import * as passport from 'passport';
 
-// import {
-//   redirectToAuthURL,
-//   authCallback,
-//   ensureAuthenticated,
-// } from '../controllers/authController';
-import * as authController from '../controllers/authController';
+import { isAuthenticated } from '../controllers/authController';
 import schema from './schema';
 
 const router = Router();
@@ -35,17 +30,19 @@ router.get(
 /** GraphQL */
 router.use(
   '/graphql',
-  authController.isAuthenticated,
+  isAuthenticated,
   bodyParser.json(),
   graphqlExpress({ schema })
 );
 
-router.use(
-  '/graphiql',
-  graphiqlExpress({
-    endpointURL: '/graphql',
-  })
-);
+if (process.env.NODE_ENV !== 'production') {
+  router.use(
+    '/graphiql',
+    graphiqlExpress({
+      endpointURL: '/graphql',
+    })
+  );
+}
 
 /** Main Pages */
 // Login Page
@@ -53,13 +50,9 @@ router.get('/login', (req: Request, res: Response) => {
   res.sendFile('login.html', { root: './dist/views/static/' });
 });
 
-// Sources Page; ensure authentication; if not, send to login.
-router.get(
-  '/',
-  authController.isAuthenticated,
-  (req: Request, res: Response) => {
-    res.sendFile('index.html', { root: './dist/views/static/' });
-  }
-);
+// Sources Page; requires authentication
+router.get('/', isAuthenticated, (req: Request, res: Response) => {
+  res.sendFile('index.html', { root: './dist/views/static/' });
+});
 
 export default router;
