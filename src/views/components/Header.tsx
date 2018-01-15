4a1a48c686a4ea4f1446803a2af9484d82bdf4b8
Nathan Smith <nathan@nathansmith.io>
2018-01-15 12:38:59 -0800

    saucy (#69)

--- src/views/components/Header.tsx
+++ src/views/components/Header.tsx
@@ -1,16 +1,38 @@
 import * as React from 'react';
-import { H1 } from 'glamorous';
+import { H1, Span } from 'glamorous';
 
-const Header = () => (
-  <H1
-    fontFamily="Futura-Medium"
-    textAlign="center"
-    color="#000"
-    fontSize="72px"
-    margin="1rem 0"
-  >
-    Sources
-  </H1>
-);
+class Header extends React.Component<{}, { sauce: boolean }> {
+  public state = {
+    sauce: false,
+  };
+
+  public handleClick = () => {
+    if (this.state.sauce) {
+      this.setState({ sauce: false });
+    } else {
+      this.setState({ sauce: true });
+    }
+  };
+
+  public render() {
+    return (
+      <H1
+        fontFamily="Futura-Medium"
+        textAlign="center"
+        color="#000"
+        fontSize="72px"
+        margin="1rem 0"
+      >
+        S
+        <Span cursor="pointer" onClick={this.handleClick}>
+          {this.state.sauce ? 'a' : 'o'}
+        </Span>
+        u
+        {this.state.sauce ? '' : 'r'}
+        ces
+      </H1>
+    );
+  }
+}
 
 export default Header;
