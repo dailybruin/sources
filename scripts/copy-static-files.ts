2072a48ffb4f22904ca524e4c9cc272f1aa86a93
Nathan Smith <nathan@nathansmith.io>
2018-04-23 18:25:51 -0700

    Static files watch vs copy

--- scripts/copy-static-files.ts
+++ scripts/copy-static-files.ts
@@ -1,11 +1,36 @@
 // src/views/{login.{html,css},*.svg}
 // src/routes/schema.graphql
 
-import { watch } from 'cpx';
+import { copy, watch } from 'cpx';
 import * as path from 'path';
 
-watch('src/routes/schema.graphql', 'dist/routes');
-watch('src/views/favicon.ico', 'dist/views');
-watch('src/views/*.svg', 'dist/views/static');
-watch('src/views/*.css', 'dist/views/static');
-watch('src/views/login.html', 'dist/views/static');
+const toCopy = [
+  {
+    src: 'src/routes/schema.graphql',
+    dest: 'dist/routes',
+  },
+  {
+    src: 'src/views/favicon.ico',
+    dest: 'dist/views',
+  },
+  {
+    src: 'src/views/*.svg',
+    dest: 'dist/views/static',
+  },
+  {
+    src: 'src/views/*.css',
+    dest: 'dist/views/static',
+  },
+  {
+    src: 'src/views/login.html',
+    dest: 'dist/views/static',
+  },
+];
+
+for (const file of toCopy) {
+  if (process.argv[2] === 'watch') {
+    watch(file.src, file.dest);
+  } else {
+    copy(file.src, file.dest);
+  }
+}
