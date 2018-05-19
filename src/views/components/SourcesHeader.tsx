92e5a68af4d511a4f73d5451dfc052e767c80a9a
Nathan Smith <nathan@nathansmith.io>
2018-05-19 12:28:30 -0700

    Header

--- src/views/components/SourcesHeader.tsx
+++ src/views/components/SourcesHeader.tsx
@@ -1,38 +1,16 @@
 import * as React from 'react';
 import { H1, Span } from 'glamorous';
 
-class SourcesHeader extends React.Component<{}, { sauce: boolean }> {
-  public state = {
-    sauce: false,
-  };
-
-  public handleClick = () => {
-    if (this.state.sauce) {
-      this.setState({ sauce: false });
-    } else {
-      this.setState({ sauce: true });
-    }
-  };
-
-  public render() {
-    return (
-      <H1
-        fontFamily="Futura-Medium"
-        textAlign="center"
-        color="#000"
-        fontSize="72px"
-        margin="1rem 0"
-      >
-        S
-        <Span cursor="pointer" onClick={this.handleClick}>
-          {this.state.sauce ? 'a' : 'o'}
-        </Span>
-        u
-        {this.state.sauce ? '' : 'r'}
-        ces
-      </H1>
-    );
-  }
-}
+const SourcesHeader = () => (
+  <H1
+    fontFamily="Futura-Medium"
+    textAlign="center"
+    color="#000"
+    fontSize="72px"
+    margin="1rem 0"
+  >
+    Squawk
+  </H1>
+);
 
 export default SourcesHeader;
