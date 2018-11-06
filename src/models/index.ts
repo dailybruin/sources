a29ce13eac31df9239bb113dd02e02e5f217f0e2
Nathan Smith <nathan@nathansmith.io>
2018-11-05 20:59:12 -0800

    Add database_url support

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@ import * as connection from 'knex'
 
 const knex = connection({
   client: 'pg',
-  connection: {
+  connection: process.env.DATABASE_URL || {
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
